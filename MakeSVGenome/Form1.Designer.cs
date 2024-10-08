9c9c710ab195ce4a4edf4f94acf4b49ab1366bc8
Dr Ian M. Carr <msjimc@leeds.ac.uk>
2024-10-08 17:10:24 +0100

    Add project files.

---
+++ MakeSVGenome/Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace MakeSVGenome
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            label5 = new Label();
+            label4 = new Label();
+            btnMakeRead = new Button();
+            nudInterval = new NumericUpDown();
+            nudlength = new NumericUpDown();
+            label3 = new Label();
+            chkRC = new CheckBox();
+            txtInsertion = new TextBox();
+            label2 = new Label();
+            btnPut = new Button();
+            txtDonor = new TextBox();
+            lblInsert = new Label();
+            btnGet = new Button();
+            btnQuit = new Button();
+            groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)nudInterval).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)nudlength).BeginInit();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            groupBox1.Controls.Add(label5);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(btnMakeRead);
+            groupBox1.Controls.Add(nudInterval);
+            groupBox1.Controls.Add(nudlength);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(chkRC);
+            groupBox1.Controls.Add(txtInsertion);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(btnPut);
+            groupBox1.Controls.Add(txtDonor);
+            groupBox1.Controls.Add(lblInsert);
+            groupBox1.Controls.Add(btnGet);
+            groupBox1.Location = new Point(12, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(663, 196);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Options";
+            // 
+            // label5
+            // 
+            label5.Anchor = AnchorStyles.Top;
+            label5.AutoSize = true;
+            label5.Location = new Point(221, 158);
+            label5.Name = "label5";
+            label5.Size = new Size(71, 15);
+            label5.TabIndex = 12;
+            label5.Text = "Interval (Kb)";
+            // 
+            // label4
+            // 
+            label4.Anchor = AnchorStyles.Top;
+            label4.AutoSize = true;
+            label4.Location = new Point(11, 158);
+            label4.Name = "label4";
+            label4.Size = new Size(94, 15);
+            label4.TabIndex = 11;
+            label4.Text = "Read length (kb)";
+            // 
+            // btnMakeRead
+            // 
+            btnMakeRead.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            btnMakeRead.Location = new Point(579, 154);
+            btnMakeRead.Name = "btnMakeRead";
+            btnMakeRead.Size = new Size(75, 23);
+            btnMakeRead.TabIndex = 10;
+            btnMakeRead.Text = "Make";
+            btnMakeRead.UseVisualStyleBackColor = true;
+            btnMakeRead.Click += btnMakeRead_Click;
+            // 
+            // nudInterval
+            // 
+            nudInterval.Anchor = AnchorStyles.Top;
+            nudInterval.Location = new Point(298, 156);
+            nudInterval.Maximum = new decimal(new int[] { 5, 0, 0, 0 });
+            nudInterval.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
+            nudInterval.Name = "nudInterval";
+            nudInterval.Size = new Size(75, 23);
+            nudInterval.TabIndex = 9;
+            nudInterval.Value = new decimal(new int[] { 1, 0, 0, 0 });
+            // 
+            // nudlength
+            // 
+            nudlength.Anchor = AnchorStyles.Top;
+            nudlength.Location = new Point(111, 156);
+            nudlength.Maximum = new decimal(new int[] { 20, 0, 0, 0 });
+            nudlength.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
+            nudlength.Name = "nudlength";
+            nudlength.Size = new Size(74, 23);
+            nudlength.TabIndex = 8;
+            nudlength.Value = new decimal(new int[] { 10, 0, 0, 0 });
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(11, 136);
+            label3.Name = "label3";
+            label3.Size = new Size(327, 15);
+            label3.TabIndex = 7;
+            label3.Text = "Create reads , set the read length and gap between each read";
+            // 
+            // chkRC
+            // 
+            chkRC.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            chkRC.AutoSize = true;
+            chkRC.CheckAlign = ContentAlignment.MiddleRight;
+            chkRC.Location = new Point(439, 43);
+            chkRC.Name = "chkRC";
+            chkRC.Size = new Size(137, 19);
+            chkRC.TabIndex = 6;
+            chkRC.Text = "Reverse complement";
+            chkRC.UseVisualStyleBackColor = true;
+            // 
+            // txtInsertion
+            // 
+            txtInsertion.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            txtInsertion.Location = new Point(8, 98);
+            txtInsertion.Name = "txtInsertion";
+            txtInsertion.Size = new Size(565, 23);
+            txtInsertion.TabIndex = 5;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(8, 76);
+            label2.Name = "label2";
+            label2.Size = new Size(114, 15);
+            label2.TabIndex = 4;
+            label2.Text = "Enter insertion point";
+            // 
+            // btnPut
+            // 
+            btnPut.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            btnPut.Location = new Point(579, 98);
+            btnPut.Name = "btnPut";
+            btnPut.Size = new Size(75, 23);
+            btnPut.TabIndex = 3;
+            btnPut.Text = "Put";
+            btnPut.UseVisualStyleBackColor = true;
+            btnPut.Click += btnPut_Click;
+            // 
+            // txtDonor
+            // 
+            txtDonor.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            txtDonor.Location = new Point(11, 41);
+            txtDonor.Name = "txtDonor";
+            txtDonor.Size = new Size(422, 23);
+            txtDonor.TabIndex = 2;
+            // 
+            // lblInsert
+            // 
+            lblInsert.AutoSize = true;
+            lblInsert.Location = new Point(11, 19);
+            lblInsert.Name = "lblInsert";
+            lblInsert.Size = new Size(340, 15);
+            lblInsert.TabIndex = 1;
+            lblInsert.Text = "Enter location of donor sequence (e.g. 100,000,000-105,000,000)";
+            // 
+            // btnGet
+            // 
+            btnGet.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            btnGet.Location = new Point(582, 41);
+            btnGet.Name = "btnGet";
+            btnGet.Size = new Size(75, 23);
+            btnGet.TabIndex = 0;
+            btnGet.Text = "Get";
+            btnGet.UseVisualStyleBackColor = true;
+            btnGet.Click += btnGet_Click;
+            // 
+            // btnQuit
+            // 
+            btnQuit.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            btnQuit.Location = new Point(591, 214);
+            btnQuit.Name = "btnQuit";
+            btnQuit.Size = new Size(75, 23);
+            btnQuit.TabIndex = 1;
+            btnQuit.Text = "Quit";
+            btnQuit.UseVisualStyleBackColor = true;
+            btnQuit.Click += btnQuit_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(687, 249);
+            Controls.Add(btnQuit);
+            Controls.Add(groupBox1);
+            Name = "Form1";
+            Text = "Make structural variants";
+            Load += Form1_Load;
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)nudInterval).EndInit();
+            ((System.ComponentModel.ISupportInitialize)nudlength).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private TextBox txtDonor;
+        private Label lblInsert;
+        private Button btnGet;
+        private Button btnQuit;
+        private CheckBox chkRC;
+        private TextBox txtInsertion;
+        private Label label2;
+        private Button btnPut;
+        private Label label5;
+        private Label label4;
+        private Button btnMakeRead;
+        private NumericUpDown nudInterval;
+        private NumericUpDown nudlength;
+        private Label label3;
+    }
+}
---
+++ MakeSVGenome/Form1.cs
@@ -0,0 +1,348 @@
+using System.Diagnostics.PerformanceData;
+using System.Text;
+using System.IO.Compression;
+using System.IO;
+using System.Diagnostics.Metrics;
+using System.Net;
+
+namespace MakeSVGenome
+{
+    public partial class Form1 : Form
+    {
+        StringBuilder insert = new StringBuilder();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnQuit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnGet_Click(object sender, EventArgs e)
+        {
+            string[] items = txtDonor.Text.Split('-');
+            if (items.Length == 0)
+            { insert = new StringBuilder(); }
+            else
+            {
+                string fileDonor = FileString.OpenAs("Select the base sequence file", "*.fa|*.fa;*.fasta");
+                if (System.IO.File.Exists(fileDonor) == false) { return; }
+
+                insert = new StringBuilder();
+
+                System.IO.StreamReader sf = null;
+                try
+                {
+                    int startPoint = -1;
+                    int endPoint = -1;
+                    startPoint = Convert.ToInt32(items[0].Replace(",", ""));
+                    endPoint = Convert.ToInt32(items[1].Replace(",", ""));
+                    if (startPoint > endPoint)
+                    {
+                        int t = startPoint;
+                        startPoint = endPoint;
+                        endPoint = t;
+                    }
+
+                    sf = new StreamReader(fileDonor);
+                    int counter = 0;
+
+                    while (sf.Peek() != 0)
+                    {
+                        char bp = (char)sf.Read();
+                        if (bp == '>')
+                        { sf.ReadLine(); }
+                        else if (char.IsLetter(bp) == true)
+                        {
+                            counter++;
+                            if (counter >= startPoint && counter <= endPoint)
+                            { insert.Append(bp); }
+                        }
+                        else if (counter > endPoint)
+                        { break; }
+                    }
+
+                    if (chkRC.Checked == true)
+                    { insert = ReverseComplement(insert); }
+                    else { insert = UpperCase(insert); }
+
+                }
+                catch (Exception ex)
+                {
+                    insert = new StringBuilder();
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+                finally { if (sf != null) { sf.Close(); } }
+
+
+                if (insert.Length > 0)
+                { lblInsert.Text = "Enter location of donor sequence (e.g. 100,000,000-105,000,000)" + " - " + insert.Length.ToString() + " bp"; }
+                else
+                { lblInsert.Text = "Enter location of donor sequence (e.g. 100,000,000-105,000,000)" + " - No sequence"; }
+            }
+        }
+
+        private StringBuilder ReverseComplement(StringBuilder sb)
+        {
+            StringBuilder RCsb = new StringBuilder(sb.Length);
+            for (int i = sb.Length - 1; i > -1; i--)
+            {
+                switch (sb[i])
+                {
+                    case 'a':
+                    case 'A':
+                        RCsb.Append("T");
+                        break;
+                    case 'c':
+                    case 'C':
+                        RCsb.Append("G");
+                        break;
+                    case 'g':
+                    case 'G':
+                        RCsb.Append("C");
+                        break;
+                    case 't':
+                    case 'T':
+                        RCsb.Append("A");
+                        break;
+                    default:
+                        RCsb.Append("N");
+                        break;
+                }
+            }
+
+            return RCsb;
+        }
+
+        private StringBuilder UpperCase(StringBuilder sb)
+        {
+            StringBuilder Uppersb = new StringBuilder(sb.Length);
+            for (int i = 0; i < sb.Length; i++)
+            {
+                switch (sb[i])
+                {
+                    case 'a':
+                    case 'A':
+                        Uppersb.Append("A");
+                        break;
+                    case 'c':
+                    case 'C':
+                        Uppersb.Append("C");
+                        break;
+                    case 'g':
+                    case 'G':
+                        Uppersb.Append("G");
+                        break;
+                    case 't':
+                    case 'T':
+                        Uppersb.Append("T");
+                        break;
+                    default:
+                        Uppersb.Append("N");
+                        break;
+                }
+            }
+
+            return Uppersb;
+        }
+
+        private void btnPut_Click(object sender, EventArgs e)
+        {
+            if (insert == null) { insert = new StringBuilder(); }
+
+            string[] items = txtInsertion.Text.Split('-');
+            if (items.Length != 2)
+            { MessageBox.Show("Most enter two locations", "Error"); }
+            else
+            {
+                string fileDonor = FileString.OpenAs("Select the base sequence file", "*.fa|*.fa;*.fasta");
+                if (System.IO.File.Exists(fileDonor) == false) { return; }
+
+                string fileSaveAs = FileString.SaveAs("Enter the name of the new reference file", "*.fa | *.fa; *.fasta");
+                if (fileSaveAs == "Cancel") { return; }
+
+                System.IO.StreamWriter sw = null;
+                System.IO.StreamReader sf = null;
+                try
+                {
+                    int startPoint = -1;
+                    int endPoint = -1;
+                    startPoint = Convert.ToInt32(items[0].Replace(",", ""));
+                    endPoint = Convert.ToInt32(items[1].Replace(",", ""));
+                    if (startPoint > endPoint)
+                    {
+                        int t = startPoint;
+                        startPoint = endPoint;
+                        endPoint = t;
+                    }
+
+                    sw = new StreamWriter(fileSaveAs);
+                    sf = new StreamReader(fileDonor);
+
+                    int counter = 0;
+
+                    while (sf.Peek() > 0)
+                    {
+                        char bp = (char)sf.Read();
+                        if (bp == '>')
+                        {
+                            sw.Write(bp + sf.ReadLine() + "\n");
+                        }
+                        else if (char.IsLetter(bp) == true)
+                        {
+                            counter++;
+                            if (counter < startPoint || counter > endPoint)
+                            { sw.Write(bp); }
+                            else if (counter == startPoint)
+                            { sw.Write(insert.ToString()); }
+                        }
+
+                        else if (char.IsLetter(bp) == true)
+                        {
+                            counter++;
+                            if (counter >= startPoint && counter <= endPoint)
+                            { insert.Append(bp); }
+                        }
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    insert = new StringBuilder();
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+                finally
+                {
+                    if (sf != null) { sf.Close(); }
+                    if (sw != null) { sw.Close(); }
+                }
+            }
+        }
+
+        private void btnMakeRead_Click(object sender, EventArgs e)
+        {
+            int length = (int)nudlength.Value * 1000;
+            int interval = (int)nudInterval.Value * 1000;
+
+
+            string folder = FileString.GetFolder("Select the folder of fasta files", "");
+            if (System.IO.Directory.Exists(folder) == false) { return; }
+
+            string[] fastas = System.IO.Directory.GetFiles(folder, "*.fasta");
+            string[] fas = System.IO.Directory.GetFiles(folder, "*.fa");
+            string[] fnas = System.IO.Directory.GetFiles(folder, "*.fna");
+
+            foreach (string s in fastas)
+            { MakeReads(s, length, interval); }
+            foreach (string s in fas)
+            { MakeReads(s, length, interval); }
+            foreach (string s in fnas)
+            { MakeReads(s, length, interval); }
+
+        }
+
+        private void MakeReads(string filename, int length, int interval)
+        {
+            string title = Text;
+
+            int index = filename.LastIndexOf('.');
+            if (index == -1) { return; }
+            
+            string output = filename.Substring(0, index) + ".fastq.gz";
+            index  = filename.LastIndexOf("\\");
+            Text = filename.Substring(index + 1);
+            Application.DoEvents();
+
+            StringBuilder wholeSequence = new StringBuilder();
+
+            System.IO.FileStream sw = null;
+            
+            try
+            {
+                string qualityString = "+\n" + new string('F', length) +"\n";
+
+                sw = new FileStream(output, FileMode.Create);
+                GZipStream gzipStream = new GZipStream(sw, CompressionMode.Compress);
+                
+                wholeSequence = getSequence(filename);
+
+                for (index = 0; index < wholeSequence.Length - length; index += interval)
+                {
+                    byte[] data = Encoding.UTF8.GetBytes("@" + Readname() + "\n" + wholeSequence.ToString(index, length) + "\n" + qualityString);
+                    gzipStream.Write(data, 0, data.Length);
+                }
+                gzipStream.Close();
+                gzipStream.Dispose();
+                sw.Close();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+            finally
+            {
+                if (sw != null) { sw.Close(); }
+                Text = title;
+            }
+
+        }
+
+        private StringBuilder getSequence(string filename)
+        {
+            StringBuilder sb = new StringBuilder();
+            System.IO.StreamReader sf = null;
+            try
+            {
+                sf = new StreamReader(filename);
+                while (sf.Peek() > 0)
+                {
+                    char bp = (char)sf.Read();
+                    if (bp == '>')
+                    {
+                        sf.ReadLine() ;
+                    }
+                    else if (char.IsLetter(bp) == true)
+                    {
+                        if ((bp == 'N' || bp == 'n')== false)
+                        { sb.Append(bp); }
+                    }                    
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+            finally
+            {
+                if (sf != null) { sf.Close(); }
+            }
+
+            return sb;
+
+        }
+
+        private string Readname()
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            StringBuilder result = new StringBuilder(25);
+            Random random = new Random();
+
+            for (int i = 0; i < 25; i++)
+            {
+                result.Append(chars[random.Next(chars.Length)]);
+            }
+
+            return result.ToString();
+        }
+
+    }
+}
